5d83afc8b7bc5974eac50fe9e329789155b6b49b
Nathan Lilienthal <nathan@nixpulvis.com>
2018-02-15 13:36:12 -0700

    Start out the job of parsing contracts with some skele.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,22 @@ extern crate syn;
 #[macro_use] extern crate quote;
 
 use proc_macro::{TokenStream, TokenNode, Delimiter, Literal};
-use syn::token;
+use syn::*;
+use syn::synom::*;
 use quote::ToTokens;
+
+// TODO: Use a real repr, not a fucking string.
+#[derive(Debug)]
+struct Contract(String);
+
+impl Synom for Contract {
+    named!(parse -> Self, value!(Contract("TODO".into())));
+}
 
 #[proc_macro_attribute]
 pub fn contract(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input: token::Fn = syn::parse(input).unwrap();
+    let args: Contract = syn::parse(args).unwrap();
+    println!("{:?}", args);
+    let input: ItemFn = syn::parse(input).unwrap();
     quote!(#input).into()
 }
